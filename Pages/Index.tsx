d7c509d1d5a86cba7e607b5deca11857cb468774
AbdulCitizen <abdulkadirgarba02@gmail.com>
2025-09-25 23:02:30 +0100

    Delete Pages directory

--- Pages/Index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold text-center">Welcome to Fayis Store 🛒</h1>
-      <p className="text-center mt-4">Browse products and shop securely with Paystack.</p>
-    </div>
-  );
-}
