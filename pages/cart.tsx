45e969aff11893259effdc0b35413b38a62fa1a2
AbdulCitizen <abdulkadirgarba02@gmail.com>
2025-09-25 17:43:17 +0100

    Create cart.tsx

---
+++ pages/cart.tsx
@@ -0,0 +1,8 @@
+export default function Cart() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Shopping Cart</h1>
+      <p>Your selected products will appear here.</p>
+    </div>
+  );
+}
